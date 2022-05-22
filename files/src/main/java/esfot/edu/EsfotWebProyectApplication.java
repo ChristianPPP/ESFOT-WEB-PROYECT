ed876aba1d8f350f240f501faf6537d5e18964ec
Christian Palacios <74626089+ChristianPPP@users.noreply.github.com>
2022-05-22 17:01:28 -0500

    Add files via upload

---
+++ files/src/main/java/esfot/edu/EsfotWebProyectApplication.java
@@ -0,0 +1,13 @@
+package esfot.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsfotWebProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsfotWebProyectApplication.class, args);
+	}
+
+}
---
+++ files/src/test/java/esfot/edu/EsfotWebProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package esfot.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsfotWebProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
